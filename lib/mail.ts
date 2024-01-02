98da9622fb908383b16ebe937f2930c26745bc38
Brian Coutts <bjhcoutts@gmail.com>
2024-01-01 18:15:01 -0800

    password reset token email complete

---
+++ lib/mail.ts
@@ -0,0 +1,40 @@
+import { createTransport } from "nodemailer"
+
+const transport = createTransport({
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
+	auth: {
+			user: process.env.MAIL_USER,
+			pass: process.env.MAIL_PASS
+	}
+} )
+
+function createEmail ( text: string )
+{
+	return `
+		<div style="
+			border: 1px solid black;
+			padding: 20px;
+			font-family: sans-serif;
+			line-height: 2;
+
+		">
+			<h2>Hi there!</h2>
+			<p>${text}</p>
+			<p>😁, Brian</p>
+		</div>
+	`
+}
+
+export async function sendPasswordResetEmail ( resetToken: string, to: string )
+{
+	const info = await transport.sendMail({
+		to,
+		from: 'test@example.com',
+		subject: 'Your password reset token!',
+		html: createEmail( `
+			<a href="${process.env.FRONTEND_URL}/reset?token=${resetToken}">Click Here to Reset!</a>
+		`)
+	} )
+	console.log(info)
+}
