b2b91e5b458d0dca612667f534f0a18de0297279
Brian Coutts <bjhcoutts@gmail.com>
2024-01-12 15:36:41 -0800

    permissions in place

--- access.ts
+++ access.ts
@@ -1,6 +1,21 @@
+import { permissionsList } from "./schemas/fields"
 import { ListAccessArgs } from "./types"
 
 export function isSignedIn ( { session }: ListAccessArgs )
 {
 	return !!session
+}
+
+export function generatedPermissions = Object.fromEntries( permissionsList.map( permission => [
+	permission,
+	function ( { session }: ListAccessArgs )
+	{
+		return !!session?.data.role?.[permission]
+	}
+]))
+
+export const permissions = {
+
+...generatedPermissions,
+
 }
