665d6887a2d66f3e50a7664c937086a6d322ac33
Brian Coutts <bjhcoutts@gmail.com>
2024-01-12 17:27:12 -0800

    productImage permissions added

--- schemas/ProductImage.ts
+++ schemas/ProductImage.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
+import { isSignedIn, permissions } from '../access'
 
 export const cloudinary = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
@@ -10,7 +11,13 @@ export const cloudinary = {
   folder: 'sickfits',
 };
 
-export const ProductImage = list({
+export const ProductImage = list( {
+  access: {
+    create: isSignedIn,
+    read: () => true,
+    update: permissions.canManageProducts,
+    delete: permissions.canManageProducts,
+  },
   fields: {
     image: cloudinaryImage({
       cloudinary,
