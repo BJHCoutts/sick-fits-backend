cac616d9472389d978272e18b3893dc678deb0fc
Brian Coutts <bjhcoutts@gmail.com>
2024-01-27 14:35:25 -0800

    typo fix

--- access.ts
+++ access.ts
@@ -1,89 +1,72 @@
-import { permissionsList } from "./schemas/fields"
-import { ListAccessArgs } from "./types"
+import { permissionsList } from './schemas/fields'
+import { ListAccessArgs } from './types'
 
-export function isSignedIn ( { session }: ListAccessArgs )
-{
+export function isSignedIn({ session }: ListAccessArgs) {
 	return !!session
 }
 
-export const generatedPermissions = Object.fromEntries( permissionsList.map( permission => [
-	permission,
-	function ( { session }: ListAccessArgs )
-	{
-		return !!session?.data.role?.[permission]
-	}
-]))
+export const generatedPermissions = Object.fromEntries(
+	permissionsList.map((permission) => [
+		permission,
+		function ({ session }: ListAccessArgs) {
+			return !!session?.data.role?.[permission]
+		},
+	])
+)
 
 export const permissions = {
 	...generatedPermissions,
-	isAwesome ( { session }: ListAccessArgs ): boolean
-	{
+	isAwesome({ session }: ListAccessArgs): boolean {
 		return session?.data.name.includes('Brian')
 	},
 }
 
 export const rules = {
-
-	canManageProducts ( { session }: ListAccessArgs)
-	{
-		if ( !isSignedIn({session}) )
-		{
+	canManageProducts({ session }: ListAccessArgs) {
+		if (!isSignedIn({ session })) {
 			return false
 		}
-		if ( permissions.canManageProducts( { session } ) )
-		{
+		if (permissions.canManageProducts({ session })) {
 			return true
 		}
 		return { user: { id: session.itemId } }
 	},
 
-	canOrder ( { session }: ListAccessArgs)
-	{
-		if ( !isSignedIn({session}) )
-		{
+	canOrder({ session }: ListAccessArgs) {
+		if (!isSignedIn({ session })) {
 			return false
 		}
-		if ( permissions.canManageCart( { session } ) )
-		{
+		if (permissions.canManageCart({ session })) {
 			return true
 		}
 		return { user: { id: session.itemId } }
 	},
 
-	canManageOrderItems ( { session }: ListAccessArgs)
-	{
-		if ( !isSignedIn({session}) )
-		{
+	canManageOrderItems({ session }: ListAccessArgs) {
+		if (!isSignedIn({ session })) {
 			return false
 		}
-		if ( permissions.canManageCart( { session } ) )
-		{
+		if (permissions.canManageCart({ session })) {
 			return true
 		}
 		return { user: { id: session.itemId } }
 	},
 
-	canReadProducts ( { session }: ListAccessArgs )
-	{
-		if ( !isSignedIn({session}) )
-		{
+	canReadProducts({ session }: ListAccessArgs) {
+		if (!isSignedIn({ session })) {
 			return false
 		}
-		if ( permissions.canManageProducts( { ession } ) )
-		{
+		if (permissions.canManageProducts({ session })) {
 			return true
 		}
 		return { status: 'AVAILABLE' }
 	},
 
-	canManageUsers ( { session }: ListAccessArgs)
-	{
-		if ( !isSignedIn({session}) )
-		{
+	canManageUsers({ session }: ListAccessArgs) {
+		if (!isSignedIn({ session })) {
 			return false
 		}
-		if ( permissions.canManageUsers( { session } ) )
-		{
+		if (permissions.canManageUsers({ session })) {
 			return true
 		}
 		return { id: session.itemId }
